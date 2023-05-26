e4c2fac79689ea8cbec6b6cc1a2277601ba12213
Aiman <aiman.ahmedpro@gmail.com>
2023-05-26 16:49:41 +0200

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      colors: {
-        primary: '#EFFAFD',
-        secondary: '#4A8BDF',
-        tertiary: '#A0006D',
-      },
-      extend: {},
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: '#EFFAFD',
+      secondary: '#4A8BDF',
+      tertiary: '#A0006D',
     },
-    plugins: [],
-  }
+    extend: {},
+  },
+  plugins: [],
+}
+
